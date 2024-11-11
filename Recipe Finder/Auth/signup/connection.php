fa867efdf6132658c4038ab9446fa1878e56cb69
umesh raut <uraut6044@gmail.com>
2024-11-11 20:48:59 +0530

    rename the file from signup to connection

--- Recipe Finder/Auth/signup/connection.php
+++ Recipe Finder/Auth/signup/connection.php
@@ -6,4 +6,10 @@ $db_name = "db1";
 
 $conn = new mysqli($servername, $username, $password, $db_name, 3306);
 
+if($conn-> connect_error){
+    die("connection failed". $conn->connect_error);
+}
+
+echo "connection sucessfull!";
+
 ?>
